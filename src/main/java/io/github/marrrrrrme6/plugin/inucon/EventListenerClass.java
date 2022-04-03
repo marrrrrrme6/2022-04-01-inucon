35514f45841dbe3d67117b73d3366e232f6c88ab
marrrrrrme6 <applepi815@gmail.com>
2022-04-03 10:12:07 +0900

    グレネード追加

---
+++ src/main/java/io/github/marrrrrrme6/plugin/inucon/EventListenerClass.java
@@ -0,0 +1,25 @@
+package io.github.marrrrrrme6.plugin.inucon;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+import io.github.marrrrrrme6.plugin.inucon.InuCon;
+
+public class EventListenerClass implements Listener {
+    @EventHandler
+    public void onHit(ProjectileHitEvent event) {
+        Projectile p = event.getEntity();
+
+        if(p instanceof Snowball && p.getName().equals("グレネード")){
+            World w = p.getWorld();
+            Location l = p.getLocation();
+
+            w.createExplosion(l, 5.0f);
+        }
+    }
+}
--- src/main/java/io/github/marrrrrrme6/plugin/inucon/InuCon.java
+++ src/main/java/io/github/marrrrrrme6/plugin/inucon/InuCon.java
@@ -1,5 +1,6 @@
 package io.github.marrrrrrme6.plugin.inucon;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InuCon extends JavaPlugin {
@@ -7,11 +8,13 @@ public final class InuCon extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("startup plugin");
+        Bukkit.getServer().getPluginManager().registerEvents(new EventListenerClass(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("shutdown plugin");
     }
 }

4b4e1c1f4bbd9844a84305688178bae0d5c1aba7
marrrrrrme6 <applepi815@gmail.com>
2022-04-03 09:35:59 +0900

    初期

---
+++ src/main/java/io/github/marrrrrrme6/plugin/inucon/InuCon.java
@@ -0,0 +1,17 @@
+package io.github.marrrrrrme6.plugin.inucon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InuCon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
